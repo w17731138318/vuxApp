28ef95c632c8bd36c4ca7fe34d5b3eaf2675856a
wwx <w17731138318@qq.com>
2018-04-21 14:33:24 +0800

    ~

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -6,5 +6,5 @@ export default function (actionName) {
   // 非生产环境 && 开启代理, 接口前缀统一使用[/proxyApi/]前缀做代理拦截!
   // console.log((process.env.NODE_ENV !== 'production' && process.env.OPEN_PROXY ? '/proxyApi/' : window.SITE_CONFIG.baseUrl) + actionName);
   
-  return 'http://127.0.0.1:8888/api' + actionName
+  return 'http://127.0.0.1:9999/api' + actionName
 }
